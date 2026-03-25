c54f36f36fd332a3d0eb9bfd2d36e93d35d3db87
Destroyerofdarkness <dawidadogier@gmail.com>
2026-03-25 13:12:40 +0100

    Fixed the updating of user bio

--- controllers/API_req/updateControllers.js
+++ controllers/API_req/updateControllers.js
@@ -3,8 +3,9 @@ const update_req = require("../../handlers/updateContentHandler")
 const update_user_bio = async(req,res)=>{
     const BODY = req.body;
     try {
-        await update_req("/update/userBio", BODY)
-        res.status(201).json({success:true});
+       const data =  await update_req("/update/userBio", BODY);
+       console.log(data)
+        res.status(201).json({success:data.success});
     } catch (err) {
         res.status(400)
     }
